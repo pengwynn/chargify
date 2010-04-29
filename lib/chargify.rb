91704288de7e2ba439cdbfff3bd90ec5046400e9
Wynn Netherland <wynn.netherland@gmail.com>
2010-04-29 09:17:58 -0500

    remove ruby gems

--- lib/chargify.rb
+++ lib/chargify.rb
@@ -1,12 +1,5 @@
-require 'rubygems'
-
-gem 'hashie', '~> 0.1.3'
 require 'hashie'
-
-gem 'httparty', '~> 0.4.5'
 require 'httparty'
-
-gem 'json', '~> 1.1.9'
 require "json"
 
 directory = File.expand_path(File.dirname(__FILE__))
